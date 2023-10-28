8fac9ba145282e9b82bd8d13a776d7eb1647d289
Kadir Kul <kkul40@gmail.com>
2023-10-28 18:17:27 -0400

    Player Controller
    Updated

--- Assets/Scripts/playerControl.cs
+++ Assets/Scripts/playerControl.cs
@@ -5,17 +5,45 @@ using UnityEngine;
 
 public class playerControl : MonoBehaviour
 {
-    public Vector2 moveDelta = new Vector2();
+    [SerializeField] private Vector2 moveDelta = new Vector2();
+    [SerializeField] private Vector2 maxForce;
+    [SerializeField] private Rigidbody2D rb2;
+    [SerializeField] private float moveForce;
+    [SerializeField] private float jumpForce;
 
-    public Rigidbody2D rb2;
-    
-    private void FixedUpdate()
+    private void Start()
+    {
+        rb2 = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
     {
         moveDelta.x = Input.GetAxis("Horizontal");
         moveDelta.y = Input.GetAxis("Vertical");
         
+        if (Input.GetKeyDown(KeyCode.Space) && !IsGrounded())
+        {
+            rb2.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Math.Abs(rb2.velocity.x) < maxForce.x)
+        {
+            rb2.AddForce(moveDelta * moveForce, ForceMode2D.Impulse);
+            // Debug.Log(rb2.velocity.y);
+        }
         
-        
-        rb2.AddForce(moveDelta, ForceMode2D.Impulse);
+        var velocity = rb2.velocity;
+        velocity.x = Math.Clamp(velocity.x, -maxForce.x, maxForce.x);
+        velocity.y = Math.Clamp(velocity.y, -maxForce.y, maxForce.y);
+
+        rb2.velocity = velocity;
+    }
+    
+    private bool IsGrounded()
+    {
+        return false;
     }
 }
