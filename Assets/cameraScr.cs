9868c389e6efa7ef6fe39511e7cf0f826d0fe3f6
Kadir Kul <kkul40@gmail.com>
2023-10-28 15:33:17 -0400

    First Init

---
+++ Assets/cameraScr.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraScr : MonoBehaviour
+{
+    public Transform iceCube;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        var icePos = iceCube.position;
+        icePos.z = -10;
+        
+        transform.position = icePos;
+    }
+}
