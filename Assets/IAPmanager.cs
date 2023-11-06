49a592a8cd3e05965fe2479132570dab295b9a36
casayona <oguz.oue40@gmail.com>
2023-11-06 02:22:09 +0100

    unity purchasing system try
    admin
    123

---
+++ Assets/IAPmanager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Purchasing;
+
+public class IAPmanager : MonoBehaviour
+{
+    private string ads = "com.celikgames.pengwin.ads";
+   public void OnPurchaseComplete(Product product)
+    {
+        if (product.definition.id == ads)
+        {
+            print("Baþarýlý");
+        }
+    }
+    public void OnPurchaseFailed(Product product, PurchaseFailureReason purchaseFailureReason)
+    {
+        if (product.definition.id == ads)
+        {
+            print("baþarýsýz");
+        }
+    }
+}
