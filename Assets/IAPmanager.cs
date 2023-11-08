6a218cf7ec8f6a74f2ad14ef2a1542e60816c30f
casayona <oguz.oue40@gmail.com>
2023-11-08 04:29:08 +0100

    PARKOUR TEST && GENEL KONTROL

--- Assets/IAPmanager.cs
+++ Assets/IAPmanager.cs
@@ -5,12 +5,15 @@ using UnityEngine.Purchasing;
 
 public class IAPmanager : MonoBehaviour
 {
+   
     private string ads = "com.celikgames.pengwin.ads";
    public void OnPurchaseComplete(Product product)
     {
         if (product.definition.id == ads)
         {
+            
             print("Baþarýlý");
+            
         }
     }
     public void OnPurchaseFailed(Product product, PurchaseFailureReason purchaseFailureReason)
