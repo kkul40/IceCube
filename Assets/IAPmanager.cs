c10c2756499dbbd575141fa8dd126b04db6bcea0
casayona <oguz.oue40@gmail.com>
2023-11-08 05:03:54 +0100

    yoruldum amk
    :alien:

--- Assets/IAPmanager.cs
+++ Assets/IAPmanager.cs
@@ -5,7 +5,12 @@ using UnityEngine.Purchasing;
 
 public class IAPmanager : MonoBehaviour
 {
-   
+
+    private void Start()
+    {
+        StandardPurchasingModule.Instance().useFakeStoreAlways = true;
+    }
+
     private string ads = "com.celikgames.pengwin.ads";
    public void OnPurchaseComplete(Product product)
     {
@@ -13,11 +18,19 @@ public class IAPmanager : MonoBehaviour
         {
             
             print("Baþarýlý");
+            removeAds();
             
         }
     }
     public void OnPurchaseFailed(Product product, PurchaseFailureReason purchaseFailureReason)
     {
         print("baþarýsýz");
+    }
+
+    private void removeAds()
+    {
+
+        GameObject.Find("ADSButton").SetActive(false);
+
     }
 }
